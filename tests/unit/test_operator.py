2556609e2c01dc2f3c13a93f3840fabd08a2b326
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-10-04 15:00:15 +0200

    skip: fix unit, lint

--- tests/unit/test_operator.py
+++ tests/unit/test_operator.py
@@ -10,7 +10,7 @@ import yaml
 from ops.model import WaitingStatus
 from ops.testing import Harness
 
-from charm import K8S_RESOURCE_FILES, TAGGED_IMAGE, NamespaceNodeAffinityOperator
+from charm import K8S_RESOURCE_FILES, NamespaceNodeAffinityOperator
 
 # Used for test_get_settings_yaml
 SETTINGS_YAML = """
@@ -48,7 +48,7 @@ class TestCharm:
     def test_context(self, harness: Harness):
         """Test context property."""
         model_name = "test-model"
-        image = TAGGED_IMAGE
+        image = harness.model.config["namespace-node-affinity-image"]
         ca_bundle = "bundle123"
         cert = "cert123"
         cert_key = "cert_key123"
