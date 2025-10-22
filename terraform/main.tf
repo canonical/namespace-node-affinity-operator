c5a9654a0f343c2d136690a7dff2a6f3d6f69c20
mvlassis <manos.vlassis@canonical.com>
2025-10-22 14:06:52 +0200

    Pinning base to 24.04

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 resource "juju_application" "namespace_node_affinity" {
   charm {
     name     = "namespace-node-affinity"
+    base     = var.base
     channel  = var.channel
     revision = var.revision
   }
